afb8934a8b7473bc6911adb0e0462a4c2972ee71
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2020-05-01 21:54:05 +0200

    First class!
    first class creation with the traditional output "Hello world"!

---
+++ src/First_Java_Programming/HelloWorld.java
@@ -0,0 +1,13 @@
+package First_Java_Programming;
+
+/**
+ *
+ * @author Fenias
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
